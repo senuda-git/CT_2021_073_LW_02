6ee36317f31dda031ddd5ec3b67d65e73eed3cfa
senuda-git <dilmith.senu@gmail.com>
2025-03-17 07:39:49 +0530

    Update Q04.java

--- LW2/src/Q_04/Q04.java
+++ LW2/src/Q_04/Q04.java
@@ -7,13 +7,11 @@ public class Q04 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter Body weight : ");
+        System.out.println("Enter Body weight in pounds : ");
         double weight = scanner.nextDouble();
-
-
 
         double calories = 19 * weight;
 
-        System.out.println("your answer is : " + calories);
+        System.out.println("your Daily Calories intake is : " + calories);
     }
 }
