1fc6497b1d20e5cdff0ab050e409c05dfeab6ab9
senuda-git <dilmith.senu@gmail.com>
2025-03-17 07:32:15 +0530

    Update d.java

--- LW2/src/Q_01/d.java
+++ LW2/src/Q_01/d.java
@@ -1,6 +1,5 @@
 package Q_01;
 
-
 import java.util.Scanner;
 
 public class d {
@@ -11,11 +10,9 @@ public class d {
         System.out.println("Enter radius : ");
         double r = scanner.nextDouble();
 
-
-
         double ANSWER = Math.PI * (Math.pow(r, 2));
 
-        System.out.println("your answer is : " + ANSWER);
+        System.out.println("your Circle Area is : " + String.format("%.2f", ANSWER));
     }
 }
 
