11c901fc08e0c1b948fb7de4a4da57d606f18a16
senuda-git <dilmith.senu@gmail.com>
2025-03-17 09:18:11 +0530

    Update Q06.java

--- LW2/src/Q_06/Q06.java
+++ LW2/src/Q_06/Q06.java
@@ -1,17 +1,21 @@
 package Q_06;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.Scanner;
 
 public class Q06 {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        GregorianCalendar calendar = new GregorianCalendar();
+        int currentYear = calendar.get(Calendar.YEAR);
 
         System.out.println("Enter the year you born in : ");
         int Year = scanner.nextInt();
 
-        int Answer = 2025 - Year;
+        int Age = currentYear - Year;
 
-        System.out.println("your were born in " + Year + " and will be " + Answer + " this year.");
+        System.out.println("your were born in " + Year + " and will be " + Age + " this year.");
     }
 }
