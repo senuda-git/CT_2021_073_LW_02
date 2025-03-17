eef0983f686dd463e1e7ec7e69f549f9e2dbc364
senuda-git <dilmith.senu@gmail.com>
2025-03-17 07:59:05 +0530

    Update Q08.java

--- LW2/src/Q_08/Q08.java
+++ LW2/src/Q_08/Q08.java
@@ -1,6 +1,5 @@
 package Q_08;
 
-
 import java.util.Scanner;
 
 public class Q08 {
@@ -11,10 +10,8 @@ public class Q08 {
         System.out.println("Enter radius : ");
         double r = scanner.nextDouble();
 
-
-
         double V = (4.0 / 3.0) * (Math.PI * (Math.pow(r , 3)));
 
-        System.out.println("your V is : " + V);
+        System.out.println("your sphere Volume is : " + String.format("%.2f", V));
     }
 }
