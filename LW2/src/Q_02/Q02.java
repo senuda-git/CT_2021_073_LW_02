e254255ab991628de71b9470cd14924d37d1ce13
senuda-git <dilmith.senu@gmail.com>
2025-03-17 07:29:26 +0530

    Update Q02.java

--- LW2/src/Q_02/Q02.java
+++ LW2/src/Q_02/Q02.java
@@ -1,21 +1,18 @@
 package Q_02;
 
-
 import java.util.Scanner;
 
 public class Q02 {
-    //c.
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter centimeter value : ");
         double cm = scanner.nextDouble();
 
+        double in = cm / 2.54;
+        int feet = (int) (in / 12);
+        in = in % 12;
 
-
-        double in = cm / 2.54 ;
-
-        System.out.println("your answer is : " + in);
+        System.out.println("It's " + feet + " feet and " + String.format("%.2f", in) + " inches.");
     }
 }
-
